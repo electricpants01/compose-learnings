791ed35ef754d3a70d9eb862dc8a2ff15d6c86eb
christian torrico <christorricoavila@gmail.com>
2026-09-10 09:13:54 -0400

    updated gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.testapp"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.example.testapp"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
